8f6cfbf0ce6daae0e30cb953add0a32a5c287533
lcatip00 <lcatip00@fesb.hr>
2021-10-12 10:03:44 +0200

    Add category controller

--- CookBook/Startup.cs
+++ CookBook/Startup.cs
@@ -10,6 +10,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using CookBookDAL;
 using Microsoft.EntityFrameworkCore;
+using CookBookDAL.Data;
 
 namespace CookBook
 {
@@ -27,6 +28,7 @@ namespace CookBook
         {
             services.AddDbContext<CookBookContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
             services.AddControllersWithViews();
         }
 
---
+++ CookBookDAL/Data/ICategoryRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CookBookDAL.Models;
+
+namespace CookBookDAL.Data
+{
+    public interface ICategoryRepository
+    {
+        Task<Category> CreateCategory(Category category);
+        Task<IEnumerable<Category>> GetCategories();
+        Task<Category> GetCategory(int categoryId);
+        Task<Category> UpdateCategory(Category category);
+        Task DeleteCategory(int categoryId);
+    }
+}
